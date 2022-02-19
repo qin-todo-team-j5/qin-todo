b86a646e12c7b7f1e9f689f420bd53cbff79f8fd
gabayt0_0@icloud.com <gabayt0_0@icloud.com>
2022-02-19 22:30:34 +0900

    yarn format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +10,6 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <p>test</p>
-
     </div>
   )
 }
