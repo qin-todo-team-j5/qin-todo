81b8c3ea7976e301017c057dfad3486243a28d8c
gabayt0_0@icloud.com <gabayt0_0@icloud.com>
2022-03-12 15:56:44 +0900

    fix tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  mode: "jit",
-  purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: 'jit',
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   content: [],
   theme: {
     extend: {},
