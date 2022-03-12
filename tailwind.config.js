d76382380c5afe029a7b6280a09e5c36b74cc932
gabayt0_0@icloud.com <gabayt0_0@icloud.com>
2022-03-12 16:29:18 +0900

    tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultColors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '1.5xl': [
+          '22px',
+          {
+            lineHeight: '26px',
+          },
+        ],
+      },
+      colors: {
+        today: defaultColors.rose[500],
+        tomorrow: defaultColors.orange[400],
+        next: defaultColors.amber[400],
+      },
+    },
   },
   plugins: [],
 }
