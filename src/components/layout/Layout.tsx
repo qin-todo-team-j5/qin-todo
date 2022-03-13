2b15d90b0ca9f4e8f91ae65f2e8d64776bb54d0c
gabayt0_0@icloud.com <gabayt0_0@icloud.com>
2022-03-13 13:22:59 +0900

    create settings header

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,15 +1,19 @@
+import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
 
 import { Header } from './Header'
+import { SettingsHeader } from './SettingsHeader'
 
 type Props = {
   children?: ReactNode
 }
 
 export const Layout = ({ children }: Props) => {
+  const router = useRouter()
+
   return (
     <div>
-      <Header />
+      {router.pathname.match(/settings/) ? <SettingsHeader /> : <Header />}
       <main>{children}</main>
     </div>
   )
